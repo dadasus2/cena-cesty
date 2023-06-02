90f6e89b39deb642dacd5090a80d9829f1956f85
Daniel <bohatec.daniel@stud.vassboskovice.cz>
2023-06-02 23:17:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function Vypocitej() {
+    const length = document.getElementById('length').value
+    const consumption = document.getElementById('consumption').value
+    const fuel = document.getElementById('fuel').value
+    let output = document.getElementById('output')
+
+
+
+    perLiter = consumption / 100
+    let price = perLiter * fuel * length
+    price = Math.round(price)
+
+    //form.reset()
+    output.textContent += `${price} Kč`
+    return price
+
+}
+
+function Smaz() {
+    let form = document.getElementById('form')
+    output.textContent = "Cena vaší cesty je:"
+    form.reset()
+}
